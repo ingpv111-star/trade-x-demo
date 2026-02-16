eb6e0a54343d45d177c82b4edde45942e9a3456c
ingpv111-star <ingpv111@gmail.com>
2026-02-16 15:18:19 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,2 @@
-self.addEventListener("install", e => {
-  self.skipWaiting();
-});
-
-self.addEventListener("fetch", e => {
-});
+self.addEventListener("install", e => { self.skipWaiting(); });
+self.addEventListener("fetch", e => {});
